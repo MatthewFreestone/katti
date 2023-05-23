749071fea1a0dc725229c34f555116fcfcd33906
Matthew Freestone <matthewfreestone1593@gmail.com>
2023-05-23 12:46:30 -0500

    fixed aliases

--- katti/main.py
+++ katti/main.py
@@ -31,7 +31,7 @@ def main():
 
     # handle args passed in
     match args.subcommand:
-        case "get":
+        case "get" | "g":
             problem_id = args.get
             preferred_language = user_conf.get("preferred_language", None)
             localproblems.get_problem(
@@ -40,17 +40,20 @@ def main():
             rating = args.random
             localproblems.get_random_problem(
                 rating, user_conf, problems_conf, kattis_conf, verbose=verbose)
-        case "run":
+        case "run" | "r":
             localproblems.run(problems_conf, verbose=verbose)
-        case "submit":
+        case "submit" | "s":
             webkattis.post(kattis_conf, user_conf, verbose=verbose)
-        case "add":
+        case "add" | "a":
             problem_id = args.add
             webkattis.add_problem(problem_id, problems_conf,
                                 kattis_conf, verbose=verbose)
-        case "description":
+        case "description" | "d":
             problem_id = args.description
             webkattis.show_description(problem_id, kattis_conf, verbose=verbose)
+        case _:
+            print(args)
+            arg_parser.print_help()
 
     # update conf files if needed
     configloader.save_user_config(USER_CONF_PATH, user_conf)
--- katti/parser.py
+++ katti/parser.py
@@ -15,27 +15,27 @@ def get_parser():
     subcommands = arg_parser.add_subparsers(title="subcommands", required=True, dest="subcommand")
 
     get_parser = subcommands.add_parser(
-        "get", help="get a kattis problem by its problem id")
+        "get", help="get a kattis problem by its problem id", aliases=["g"])
     _add_verbose(get_parser)
     get_parser.add_argument(
         "get", metavar="<problem-id>", help="get a kattis problem by its problem id", type=str)
     
     run_parser = subcommands.add_parser(
-        "run", help="run the test cases for a given problem")
+        "run", help="run the test cases for a given problem", aliases=["r"])
     _add_verbose(run_parser)
     
     submit_parser = subcommands.add_parser(
-        "submit", help="submit a kattis problem")
+        "submit", help="submit a kattis problem", aliases=["s"])
     _add_verbose(submit_parser)
 
     description_parser = subcommands.add_parser(
-        "description", help="display a problem's description in chrome")
+        "description", help="display a problem's description in chrome", aliases=["d"])
     _add_verbose(description_parser)
     description_parser.add_argument(
         "description", metavar="<problem-id>", help="display a problem's description in chrome", type=str)
 
     add_parser = subcommands.add_parser(
-        "add", help="add a problem id to your problem config file")
+        "add", help="add a problem id to your problem config file", aliases=["a"])
     _add_verbose(add_parser)
     add_parser.add_argument(
         "add", metavar="<problem-id>", help="add a problem id to your problem config file", type=str)
