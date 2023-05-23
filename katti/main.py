8c419cb3d617acbe3379e6a5b26bbca0f1d4ff55
Matthew Freestone <matthewfreestone1593@gmail.com>
2023-05-23 12:00:18 -0500

    added subparsers

--- katti/main.py
+++ katti/main.py
@@ -2,9 +2,8 @@ import pkg_resources
 import os
 import sys
 import json
-import argparse
 import datetime
-from katti import webkattis, configloader, localproblems
+from katti import webkattis, configloader, localproblems, parser
 from katti.constants import colors
 
 
@@ -18,13 +17,6 @@ KATTIS_CONF_PATH = os.path.join(os.path.expanduser('~'), '.kattisrc')
 
 # for customization of arg parser
 
-
-class Parser(argparse.ArgumentParser):
-    def _check_value(self, action, value):
-        if action.choices is not None and value not in action.choices:
-            raise argparse.ArgumentError(action, "invalid option")
-
-
 def main():
     # TODO these 3 operations are potentially blocking, consider moving to separate threads
     user_conf = configloader.load_user_config(USER_CONF_PATH)
@@ -32,71 +24,33 @@ def main():
     kattis_conf = configloader.get_kattis_config(KATTIS_CONF_PATH)
 
     # add command line args
-    arg_parser = Parser(
-        prog="katti", description="A command line tool testing and submitting kattis problems.")
-    arg_parser.add_argument(
-        "-g",
-        "--get",
-        metavar="<problem-id>",
-        help="get a kattis problem by its problem id",
-        type=str,
-        # choices=list(problems_conf.keys())
-    )
-    arg_parser.add_argument(
-        "-r", "--run", help="run the test cases for a given problem", action="store_true")
-    arg_parser.add_argument(
-        "-p", "--post", help="submit a kattis problem", action="store_true")
-    arg_parser.add_argument(
-        "-v", "--verbose", help="receive verbose outputs", action="store_true")
-    arg_parser.add_argument(
-        "-d", "--description", help="display a problem's description in chrome", metavar="<problem-id>")
-    arg_parser.add_argument("--add", metavar="<problem_id>",
-                            help="add a problem id to your problem config file")
-    arg_parser.add_argument("--random", metavar="<rating>",
-                            help="selects a random added kattis problem with a given rating")
-    arg_parser.add_argument(
-        "--stats", help="get kattis stats if possible", action="store_true")
-    arg_parser.add_argument(
-        "--history", help="see your 50 most recent kattis submissions", action="store_true")
-    arg_parser.add_argument("--history_size", metavar="<size>",
-                            help="set history size with a number and query history size with -1")
-    arg_parser.add_argument("--update_period", metavar="<hours>",
-                            help="set how frequently katti updates problem ratings in hours")
-    arg_parser.add_argument("--update_zsh_completions",
-                            help="update katti completions for zsh users", action="store_true")
+    arg_parser = parser.get_parser()
     args = arg_parser.parse_args()
     # track verbosity
     verbose = args.verbose
+
     # handle args passed in
-    if args.get:
-        preferred_language = user_conf.get("preferred_language", None)
-        localproblems.get_problem(
-            args.get, problems_conf, kattis_conf, preferred_language, verbose=verbose)
-    elif args.random:
-        rating = args.random
-        localproblems.get_random_problem(
-            rating, user_conf, problems_conf, kattis_conf, verbose=verbose)
-    elif args.run:
-        localproblems.run(problems_conf, verbose=verbose)
-    elif args.post:
-        webkattis.post(kattis_conf, user_conf, verbose=verbose)
-    elif args.add:
-        problem_id = args.add
-        webkattis.add_problem(problem_id, problems_conf,
-                              kattis_conf, verbose=verbose)
-    elif args.description:
-        problem_id = args.description
-        webkattis.show_description(problem_id, kattis_conf, verbose=verbose)
-    # elif args.stats:
-    #     get_stats()
-    # elif args.history:
-    #     get_history()
-    # elif args.history_size:
-    #     handle_history_size(args.history_size)
-    # elif args.update_period:
-    #     set_update_period(args.update_period)
-    # elif args.update_zsh_completions:
-    #     update_zsh_completions()
+    match args.subcommand:
+        case "get":
+            problem_id = args.get
+            preferred_language = user_conf.get("preferred_language", None)
+            localproblems.get_problem(
+                problem_id, problems_conf, kattis_conf, preferred_language, verbose=verbose)
+        case "random":
+            rating = args.random
+            localproblems.get_random_problem(
+                rating, user_conf, problems_conf, kattis_conf, verbose=verbose)
+        case "run":
+            localproblems.run(problems_conf, verbose=verbose)
+        case "submit":
+            webkattis.post(kattis_conf, user_conf, verbose=verbose)
+        case "add":
+            problem_id = args.add
+            webkattis.add_problem(problem_id, problems_conf,
+                                kattis_conf, verbose=verbose)
+        case "description":
+            problem_id = args.description
+            webkattis.show_description(problem_id, kattis_conf, verbose=verbose)
 
     # update conf files if needed
     configloader.save_user_config(USER_CONF_PATH, user_conf)
---
+++ katti/parser.py
@@ -0,0 +1,50 @@
+import argparse
+
+class Parser(argparse.ArgumentParser):
+    def _check_value(self, action, value):
+        if action.choices is not None and value not in action.choices:
+            raise argparse.ArgumentError(action, "invalid option")
+        
+def _add_verbose(parser: Parser): 
+    parser.add_argument("-v", "--verbose", help="receive verbose outputs", action="store_true")
+
+def get_parser():
+    # add command line args
+    arg_parser = Parser(
+        prog="katti", description="A command line tool testing and submitting kattis problems.")
+    subcommands = arg_parser.add_subparsers(title="subcommands", required=True, dest="subcommand")
+
+    get_parser = subcommands.add_parser(
+        "get", help="get a kattis problem by its problem id")
+    _add_verbose(get_parser)
+    get_parser.add_argument(
+        "get", metavar="<problem-id>", help="get a kattis problem by its problem id", type=str)
+    
+    run_parser = subcommands.add_parser(
+        "run", help="run the test cases for a given problem")
+    _add_verbose(run_parser)
+    
+    submit_parser = subcommands.add_parser(
+        "submit", help="submit a kattis problem")
+    _add_verbose(submit_parser)
+
+    description_parser = subcommands.add_parser(
+        "description", help="display a problem's description in chrome")
+    _add_verbose(description_parser)
+    description_parser.add_argument(
+        "description", metavar="<problem-id>", help="display a problem's description in chrome", type=str)
+
+    add_parser = subcommands.add_parser(
+        "add", help="add a problem id to your problem config file")
+    _add_verbose(add_parser)
+    add_parser.add_argument(
+        "add", metavar="<problem-id>", help="add a problem id to your problem config file", type=str)
+
+    random_parser = subcommands.add_parser(
+        "random", help="selects a random added kattis problem with a given rating")
+    _add_verbose(random_parser)
+    random_parser.add_argument(
+        "random", metavar="<rating>", help="selects a random added kattis problem with a given rating", type=str)
+    
+    
+    return arg_parser
